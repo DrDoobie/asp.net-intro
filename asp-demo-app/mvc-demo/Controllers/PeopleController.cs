5a0e270e50a6057c1dd3f1013793c0a5604b6ec7
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-21 01:02:15 +0200

    Init

---
+++ asp-demo-app/mvc-demo/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using mvc_demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_demo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople ()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+   
+            people.Add(new PersonModel {firstName = "Willie", lastName = "Nelson", birthYear = 1933});
+            people.Add(new PersonModel {firstName = "Bobbie", lastName = "Jackson", birthYear = 1968});
+            people.Add(new PersonModel {firstName = "Bill", lastName = "Wesley", birthYear = 1754});
+
+            return View(people);
+        }
+    }
+}
---
+++ asp-demo-app/mvc-demo/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_demo.Models
+{
+    public class PersonModel {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int birthYear { get; set; }  
+        public bool isLiving { get; set; } = true;
+    }
+}
